1c3c02e822e451b4045acfcb1a04453ffe6c3273
lalfonso <lualfonse@gmail.com>
2020-02-25 18:10:57 -0300

    end of day

--- yaml2raml.rb
+++ yaml2raml.rb
@@ -68,49 +68,75 @@ def render_templates
         #    p "Creating file success" 
         end
 end
-def process_models
+def process_entities
     @models = {}
+
     @file_yaml["entities"].each do |name, type|
-        process_struc name, type if type.is_a?Hash
+        process_entities_struc name, type,Hash.new if type.is_a?Hash            
     end
+    @models
+    _resources
 end
-def process_struc parm_name, parm_type
-    
-    properties = {}
+def _resources
+    @resources = {}
+
+    @file_yaml["entities"].each do |name, props|   
+        @resources[name] = process_resources(name,props)
+    end
+    p @resources
+end
+def process_resources  name,props
+        resource = {}  
+        pk = ""
+        props.each do |type_name, type_props|
+            pk = type_name if type_name.include?"*"
+            resource[type_name] = process_resources(type_name,type_props) if type_props.is_a?Hash
+        end
+        {domain:resource,  pk:pk}
+end
+def process_entities_struc parm_name, parm_type, properties
+    properties_default = {}
+    properties_default = properties_default.merge(properties)
+    parm_type.select{|name, type| name.include?"*" }.each do |name, type|
+        properties_default[parm_name + "_" + name] = "ref:#{parm_name}.#{name.gsub("*","")}" 
+    end
     parm_type.each do |name, type|
-        properties[name] = {type: type}
-        properties[name] = {type: name} if type.is_a?Hash
-        process_struc (parm_name + "_" + name), type if type.is_a?Hash
+        properties[name] = type unless type.is_a?Hash
+        process_entities_struc((parm_name + "_" + name), type, properties_default) if type.is_a?Hash
     end
     @models[parm_name] = properties
 end
-def process_types
+def process_entities_types
     @model_types = {}
     @file_yaml["entities"].each do |name, type|
-        process_struc_types name, type
+        process_struc_types name, type, {}
     end
     p @model_types
 end
 
-def process_struc_types parm_name, parm_type
+def process_struc_types parm_name, parm_type, defaults
+    defaults.each do |name, type|
+        @model_types[parm_name + "_" + name] = type unless type.is_a?Hash
+    end
     parm_type.each do |name, type|
         @model_types[parm_name + "_" + name] = type unless type.is_a?Hash
-        process_struc_types (parm_name + "_" + name), type if type.is_a?Hash
+        defaults[parm_name + "_" + name] = type if name.include?"*"
+        process_struc_types (parm_name + "_" + name), type,defaults  if type.is_a?Hash
     end
-
+    p defaults
 end
 op = OptionParser.new
 op.on("-f name") do |file|
     p "creando RAML para #{file}"
     @file_yaml = YAML.load_file(file) 
     @artifact = @file_yaml["application"]["name"]
-    process_models
-    process_types
-    @file_yaml["microservice"].each do |entity|
+    process_entities
+    process_entities_types
+    @models.each do |entity, props|
         @entity = entity.camelize
         @entities = @entity.pluralize
         @attributes= []
-        @file_yaml["entities"][entity].each do |name , type|
+        props.each do |name , type|
             if !type.is_a?(Hash)
             @entity_pk = (entity+"_"+name.gsub("*","").gsub("[]","")).camelize if name.include?"*"
             @attributes.push ({pk: name.include?("*")  , name: (entity+"_"+name.gsub("*","").gsub("[]","")).camelize , type: type.split(" ")[0], identity: type.include?("identity") })
